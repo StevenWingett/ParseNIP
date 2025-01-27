08712eaaa5183f9269bf413355d5755ec3a577a8
StevenWingett <steven.wingett@mrc-lmb.cam.ac.uk>
2025-01-27 15:27:57 +0000

    Added Python script to perform basic QC of single cell data using Scanpy

---
+++ bin/scanpy_qc.py
@@ -0,0 +1,130 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # Import of matrix data, filtering and Quality Control using Scapy following the Parse Bioscienes tutorial
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Loading libraries and setting the location path for analysis data
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import scipy
+import os
+import scipy.io as sio
+
+data_path = 'result_copy/splitpipe_combined_sublibraries/SampleA/DGE_unfiltered/'
+
+# oras://community.wave.seqera.io/library/pip_igraph_leidenalg_scanpy:ae205e6e609e6292
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Adjusting Scanpy default settings
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+sc.settings.verbosity = 1 # verbosity: errors (0), warnings (1), info (2), hints (3)
+sc.settings.set_figure_params(dpi=100, fontsize=10, dpi_save=300, figsize=(5,4), format='png')
+sc.settings.figdir = '/mnt/output_figures/'
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Reading in data
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# The DGE_filtered folder contains the expression matrix, genes, and files
+# NOTE: split-pipe versions older than 1.1.0 used 'DGE.mtx'
+adata = sc.read_mtx(data_path + 'count_matrix.mtx')
+
+# reading in gene and cell data
+gene_data = pd.read_csv(data_path + 'all_genes.csv')
+cell_meta = pd.read_csv(data_path + 'cell_metadata.csv')
+
+# find genes with nan values and filter
+gene_data = gene_data[gene_data.gene_name.notnull()]
+notNa = gene_data.index
+notNa = notNa.to_list()
+
+# remove genes with nan values and assign gene names
+adata = adata[:,notNa]
+adata.var = gene_data
+adata.var.set_index('gene_name', inplace=True)
+adata.var.index.name = None
+adata.var_names_make_unique()
+
+# add cell meta data to anndata object
+adata.obs = cell_meta
+adata.obs.set_index('bc_wells', inplace=True)
+adata.obs.index.name = None
+adata.obs_names_make_unique()
+
+# Returns the dimensions of the expression matrix (cells, genes)
+print(adata.shape)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Cell quality control
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+adata.var['mt'] = adata.var_names.str.startswith('MT-')
+sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+
+# Scanpy will prepend the string in the save argument with "violin"
+# and save it to our figure directory defined in the first step.
+sc.pl.violin(adata, ['n_genes_by_counts'], save='_n_genes', jitter=0.4)
+sc.pl.violin(adata, ['total_counts'], save='_total_counts', jitter=0.4)
+sc.pl.violin(adata, ['pct_counts_mt'], save='_mito_pct', jitter=0.4)
+
+# Filter the data
+adata = adata[adata.obs.n_genes_by_counts < 5000,:]
+adata = adata[adata.obs.total_counts < 20000,:]
+adata = adata[adata.obs.pct_counts_mt < 15,:]
+adata.shape # Checking number of cells remaining
+
+sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts', save='_gene_vs_transcript_counts')
+print('median transcript count per cell: ' + str(adata.obs['tscp_count'].median(0)))
+print('median gene count per cell: ' + str(adata.obs['gene_count'].median(0)))
+
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
+
+sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.25)
+sc.pl.highly_variable_genes(adata, save='') # scanpy generates the filename automatically
+
+# Save raw expression values before variable gene subset
+adata.raw = adata
+
+adata = adata[:, adata.var.highly_variable]
+sc.pp.regress_out(adata, ['total_counts'])
+sc.pp.scale(adata, max_value=10)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Principal component analysis
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+sc.tl.pca(adata, svd_solver='arpack')
+sc.pl.pca_variance_ratio(adata, log=True, n_pcs=50, save='') # scanpy generates the filename automatically
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# UMAP and Leiden Clustering
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+sc.pp.neighbors(adata, n_neighbors=10, n_pcs=30)
+sc.tl.umap(adata)
+sc.tl.leiden(adata, resolution=0.5)
+sc.pl.umap(adata, color=['leiden'], legend_fontsize=8, save='_leiden')
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Finding cluster markers
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+sc.tl.rank_genes_groups(adata, 'leiden', method='t-test')
+
+# The head function returns the top n genes per cluster
+top_markers = pd.DataFrame(adata.uns['rank_genes_groups']['names']).head(5)
+print(top_markers)
+
+print('Done')
