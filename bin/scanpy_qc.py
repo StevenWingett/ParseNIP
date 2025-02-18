9db38fd1c82dca91e3649a9fb65bb8639851ac91
Steven Wingett <9609778+StevenWingett@users.noreply.github.com>
2025-02-18 11:30:07 +0000

    Add try/except so script fails more gracefully
    Failure seems to occur when the input dataset is a small size and UMAPs cannot be generated.
    
    Without this check, the failure to create a UMAP will cause the Nextflow pipeline to terminate, which is not desired, since the input data may actually be very small.

--- bin/scanpy_qc.py
+++ bin/scanpy_qc.py
@@ -54,98 +54,116 @@ else:
 
 # The DGE_filtered folder contains the expression matrix, genes, and files
 # NOTE: split-pipe versions older than 1.1.0 used 'DGE.mtx'
-print(data_path)
-adata = sc.read_mtx(data_path + 'count_matrix.mtx')
+#print(data_path)
 
-# reading in gene and cell data
-gene_data = pd.read_csv(data_path + 'all_genes.csv')
-cell_meta = pd.read_csv(data_path + 'cell_metadata.csv')
+try:
+    adata = sc.read_mtx(data_path + 'count_matrix.mtx')
 
-# find genes with nan values and filter
-gene_data = gene_data[gene_data.gene_name.notnull()]
-notNa = gene_data.index
-notNa = notNa.to_list()
+    # reading in gene and cell data
+    gene_data = pd.read_csv(data_path + 'all_genes.csv')
+    cell_meta = pd.read_csv(data_path + 'cell_metadata.csv')
+except:
+  print("Could not import data!")
+  exit(0)
 
-# remove genes with nan values and assign gene names
-adata = adata[:,notNa]
-adata.var = gene_data
-adata.var.set_index('gene_name', inplace=True)
-adata.var.index.name = None
-adata.var_names_make_unique()
 
-# add cell meta data to anndata object
-adata.obs = cell_meta
-adata.obs.set_index('bc_wells', inplace=True)
-adata.obs.index.name = None
-adata.obs_names_make_unique()
+try:
+    # find genes with nan values and filter
+    gene_data = gene_data[gene_data.gene_name.notnull()]
+    notNa = gene_data.index
+    notNa = notNa.to_list()
 
-# Returns the dimensions of the expression matrix (cells, genes)
-print(adata.shape)
+    # remove genes with nan values and assign gene names
+    adata = adata[:,notNa]
+    adata.var = gene_data
+    adata.var.set_index('gene_name', inplace=True)
+    adata.var.index.name = None
+    adata.var_names_make_unique()
+
+    # add cell meta data to anndata object
+    adata.obs = cell_meta
+    adata.obs.set_index('bc_wells', inplace=True)
+    adata.obs.index.name = None
+    adata.obs_names_make_unique()
+
+    # Returns the dimensions of the expression matrix (cells, genes)
+    print(adata.shape)
+except:
+      print("Could not filter data!")
+      exit(0)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Cell quality control
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-adata.var['mt'] = adata.var_names.str.startswith('MT-')
-sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+try:
+    adata.var['mt'] = adata.var_names.str.startswith('MT-')
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
 
-# Scanpy will prepend the string in the save argument with "violin"
-# and save it to our figure directory defined in the first step.
-sc.pl.violin(adata, ['n_genes_by_counts'], save='_n_genes', jitter=0.4)
-sc.pl.violin(adata, ['total_counts'], save='_total_counts', jitter=0.4)
-sc.pl.violin(adata, ['pct_counts_mt'], save='_mito_pct', jitter=0.4)
+    # Scanpy will prepend the string in the save argument with "violin"
+    # and save it to our figure directory defined in the first step.
+    sc.pl.violin(adata, ['n_genes_by_counts'], save='_n_genes', jitter=0.4)
+    sc.pl.violin(adata, ['total_counts'], save='_total_counts', jitter=0.4)
+    sc.pl.violin(adata, ['pct_counts_mt'], save='_mito_pct', jitter=0.4)
 
-# Filter the data
-adata = adata[adata.obs.n_genes_by_counts < 5000,:]
-adata = adata[adata.obs.total_counts < 20000,:]
-adata = adata[adata.obs.pct_counts_mt < 15,:]
-adata.shape # Checking number of cells remaining
+    # Filter the data
+    adata = adata[adata.obs.n_genes_by_counts < 5000,:]
+    adata = adata[adata.obs.total_counts < 20000,:]
+    adata = adata[adata.obs.pct_counts_mt < 15,:]
+    adata.shape # Checking number of cells remaining
 
-sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts', save='_gene_vs_transcript_counts')
-print('median transcript count per cell: ' + str(adata.obs['tscp_count'].median(0)))
-print('median gene count per cell: ' + str(adata.obs['gene_count'].median(0)))
+    sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts', save='_gene_vs_transcript_counts')
+    print('median transcript count per cell: ' + str(adata.obs['tscp_count'].median(0)))
+    print('median gene count per cell: ' + str(adata.obs['gene_count'].median(0)))
 
-sc.pp.normalize_total(adata, target_sum=1e4)
-sc.pp.log1p(adata)
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
 
-sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.25)
-sc.pl.highly_variable_genes(adata, save='') # scanpy generates the filename automatically
+    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.25)
+    sc.pl.highly_variable_genes(adata, save='') # scanpy generates the filename automatically
 
-# Save raw expression values before variable gene subset
-adata.raw = adata
+    # Save raw expression values before variable gene subset
+    adata.raw = adata
 
-adata = adata[:, adata.var.highly_variable]
-sc.pp.regress_out(adata, ['total_counts'])
-sc.pp.scale(adata, max_value=10)
+    adata = adata[:, adata.var.highly_variable]
+    sc.pp.regress_out(adata, ['total_counts'])
+    sc.pp.scale(adata, max_value=10)
+except:
+    print("Could not perform cell quality control")
+    exit(0)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Principal component analysis
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-sc.tl.pca(adata, svd_solver='arpack')
-sc.pl.pca_variance_ratio(adata, log=True, n_pcs=50, save='') # scanpy generates the filename automatically
+try:
+    sc.tl.pca(adata, svd_solver='arpack')
+    sc.pl.pca_variance_ratio(adata, log=True, n_pcs=50, save='') # scanpy generates the filename automatically
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# UMAP and Leiden Clustering
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+    # UMAP and Leiden Clustering
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-sc.pp.neighbors(adata, n_neighbors=10, n_pcs=30)
-sc.tl.umap(adata)
-sc.tl.leiden(adata, resolution=0.5)
-sc.pl.umap(adata, color=['leiden'], legend_fontsize=8, save='_leiden')
+    sc.pp.neighbors(adata, n_neighbors=10, n_pcs=30)
+    sc.tl.umap(adata)
+    sc.tl.leiden(adata, resolution=0.5)
+    sc.pl.umap(adata, color=['leiden'], legend_fontsize=8, save='_leiden')
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# Finding cluster markers
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+    # Finding cluster markers
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-sc.tl.rank_genes_groups(adata, 'leiden', method='t-test')
+    sc.tl.rank_genes_groups(adata, 'leiden', method='t-test')
 
-# The head function returns the top n genes per cluster
-top_markers = pd.DataFrame(adata.uns['rank_genes_groups']['names']).head(5)
-print(top_markers)
+    # The head function returns the top n genes per cluster
+    top_markers = pd.DataFrame(adata.uns['rank_genes_groups']['names']).head(5)
+    print(top_markers)
+except:
+    print("Could not perform dimensionality reduction")
+
 
 print('Done')
